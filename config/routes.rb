854dd69e2381257d3374957269ea933eea3afea3
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-14 19:48:22 +0545

    Add Application Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/contact'
-
   devise_for :users
   resources :websites
+
+  get '/contact', to: 'static_pages#contact'
   root to: 'static_pages#index'
 end
