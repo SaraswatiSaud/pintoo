85a97b3d20bb4a973948886321879e004232b05c
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-14 20:19:16 +0545

    Create dashboard page and add beautiful flash messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, only: :dashboard
+
   def index
   end
 
   def contact
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :websites
 
   get '/contact', to: 'static_pages#contact'
+  get '/dashboard', to: 'static_pages#dashboard'
   root to: 'static_pages#index'
 end
