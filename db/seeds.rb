73cdc2eac92e2cd2b42938d575472e6a1ec0a975
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-15 17:36:33 +0545

    Add seed for creating default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.where(email: 'admin@example.com').first_or_create!(password: 'password')
