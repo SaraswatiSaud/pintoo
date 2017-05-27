864439961afde96195da87a391368450f1a67490
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-27 10:50:38 +0545

    Rename Test Site Controller

--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class WebsitesControllerControllerTest < ActionDispatch::IntegrationTest
+class SitesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
