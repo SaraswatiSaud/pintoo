d2cb1ba4676a46b7bb21a4c6e18ea8a0c5d35fd2
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-31 12:27:10 +0545

    Created destroy action and related views of website

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -26,6 +26,12 @@ class WebsitesController < ApplicationController
     redirect_to websites_path
   end
 
+  def destroy
+    @website = current_user.websites.find(params[:id])
+    @website.destroy
+    redirect_to websites_path
+  end
+
   private
   def website_params
     params.require(:website).permit(:name, :subdomain)
