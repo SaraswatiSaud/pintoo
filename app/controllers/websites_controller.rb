4456725ada0f6ad1793989fefdbce3bd00dadb5c
Saraswati Saud <saraswati.saud@gmail.com>
2017-05-31 12:01:02 +0545

    Created new action and related views of a website

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -7,9 +7,17 @@ class WebsitesController < ApplicationController
   end
 
   def create
+    @website = current_user.websites.new(website_params)
+    @website.save
+    redirect_to @website
   end
 
   def show
     @website = current_user.websites.find(params[:id])
+  end
+
+  private
+  def website_params
+    params.require(:website).permit(:name, :subdomain)
   end
 end
