6417b6696adce011573a398f9d1d8e3176a9808b
SaraswatiSaud <saraswati.saud@gmail.com>
2017-06-29 05:57:47 +0545

    Updated seeds file with user websites

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.where(email: 'admin@example.com').first_or_create!(password: 'password')
+user = User.where(email: 'admin@example.com').first_or_create!(password: 'password')
+user.websites.where(name: 'Website 1', subdomain: 'website1').first_or_create!
+user.websites.where(name: 'Website 2', subdomain: 'website2').first_or_create!
