998ac4b0fefeccb52e1dc1af2eef4bce67c4f2e2
Saraswati Saud <saraswati.saud@gmail.com>
2017-07-14 22:39:44 +0545

    Rspec validation for model user.rb

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,5 +1,5 @@
 class Website < ApplicationRecord
+  validates :name, :subdomain, presence: true, length: { minimum: 5 }, uniqueness: true
+  validates :user_id, presence: true
   belongs_to :user
-  validates :name, presence: true, length: { minimum: 5 } 
-  validates :subdomain, presence: true
 end
