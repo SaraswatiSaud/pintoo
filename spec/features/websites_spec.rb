14831e2124070f5b385a1dcbd4ab4b68c22da6a3
Saraswati Saud <saraswati.saud@gmail.com>
2017-08-01 21:45:53 +0545

    Rspec test for websites page

--- spec/features/websites_spec.rb
+++ spec/features/websites_spec.rb
@@ -1,24 +1,51 @@
 require 'rails_helper'
 
 RSpec.feature "Websites", type: :feature do
-  scenario 'user creates a new website' do
-    user = FactoryGirl.create(:user)
+  before(:each) do
+    @user = FactoryGirl.create(:user)
 
     visit root_path
     click_link 'Log In'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
     click_button 'Log in'
+  end
 
+  scenario 'user creates a new website' do
     expect {
       click_link 'Websites'
       click_link 'Add Website'
       fill_in 'Name', with: 'Website 1'
       fill_in 'Subdomain', with: 'website1'
       click_button 'Save Website'
-    }.to change(user.websites, :count).by(1)
+    }.to change(@user.websites, :count).by(1)
 
     expect(page).to have_content 'Website 1'
     expect(page).to have_content 'website1'
+  end
+
+  scenario 'user creates a show page' do
+    website = FactoryGirl.create(:website, user: @user)
+    visit websites_path
+    click_link "#{website.name}"
+
+    expect(page).to have_content "#{website.name}"
+  end
+
+  scenario 'user edits an article' do
+    website = FactoryGirl.create(:website, user: @user)
+    visit edit_website_path(website)
+    expect {
+      fill_in 'Name', with: 'Edit name'
+      fill_in 'Subdomain', with: 'Edit subdomain'
+      click_button 'Update Website'
+    }.to_not change(@user.websites, :count)
+  end
+
+  scenario 'user logout the page' do
+    website = FactoryGirl.create(:website, user: @user)
+    visit websites_path
+    click_link 'Logout'
+    expect(page).to have_content 'Signed out successfully.'
   end
 end
