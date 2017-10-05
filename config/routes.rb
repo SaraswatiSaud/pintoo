1cf3dadb8176878971028250f1ea5f0f7aa27eb1
Lalu Saud <lalusaud@gmail.com>
2017-10-05 00:04:18 -0400

    Update UI enhancements

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :menus
   resources :pages
 
-
   get '/contact', to: 'static_pages#contact'
   get '/dashboard', to: 'static_pages#dashboard'
+  get '/settings', to: 'static_pages#settings'
   root to: 'static_pages#index'
 end
