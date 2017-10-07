d4d1d25dd3bd0e61d3ea8c4bd496cf5dae95796c
Saraswati Saud <saraswati.saud@gmail.com>
2017-10-07 11:16:01 +0545

    Associate page with website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,38 +1,48 @@
 class PagesController < ApplicationController
+  def new
+    @website = Website.friendly.find(params[:website_id])
+    @page = @website.pages.new
+  end
+
   def create
-    @website = Website.find(params[:website_id])
+    @website = Website.friendly.find(params[:website_id])
     @page = @website.pages.new(page_params)
     if @page.save
-      redirect_to pages_path
+      redirect_to website_page_path(@website, @page)
     else
       render 'new'
     end
   end
 
+  def show
+    @website = Website.friendly.find(params[:website_id])
+    @page = @website.pages.friendly.find(params[:id])
+  end
+
   def edit
-    @website = Website.find(params[:website_id])
-    @page = @website.pages.find(params[:id])
+    @website = Website.friendly.find(params[:website_id])
+    @page = @website.pages.friendly.find(params[:id])
   end
 
   def update
-    @website = Website.find(params[:website_id])
-    @page = Page.find(params[:id])
+    @website = Website.friendly.find(params[:website_id])
+    @page = Page.friendly.find(params[:id])
     if @page.update(page_params)
-      redirect_to pages_path
+      redirect_to website_path(@website)
     else
       render 'edit'
     end
   end
 
   def destroy
-    @website = Website.find(params[:website_id])
-    @page = Page.find(params[:id])
+    @website = Website.friendly.find(params[:website_id])
+    @page = Page.friendly.find(params[:id])
     @page.destroy
-    redirect_to pages_path
+    redirect_to website_path(@website)
   end
 
   private
   def page_params
-    params.require(:page).permit(:name, :path)
+    params.require(:page).permit(:name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004152301) do
+ActiveRecord::Schema.define(version: 20171007032337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 20171004152301) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "website_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_pages_on_slug"
   end
 
   create_table "products", id: :integer, default: nil, force: :cascade do |t|
