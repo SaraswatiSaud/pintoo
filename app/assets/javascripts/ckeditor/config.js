edea23be50926b52ff1bc745ae90b1744c05d627
Saraswati Saud <saraswati.saud@gmail.com>
2017-11-19 23:06:35 +0545

    Generate dynamic title on a per-page basis

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,7 @@
+CKEDITOR.editorConfig = function (config) {
+  config.toolbar_mini = [
+    ["Bold", "Italic", "Underline", "Strike", "-", "Subscript", "Superscript"],
+    ["Font Size"]
+  ];
+  config.toolbar = "mini"
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     when :alert then "alert alert-danger"
     end
   end
+
+  def full_title(page_title = '')
+    base_title = "ResFrame"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
