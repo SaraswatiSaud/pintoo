4cc3591da78f07a3ff21ba747743d8029c6023db
Saraswati Saud <saraswati.saud@gmail.com>
2017-11-27 11:30:56 +0545

    Email validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save { email.downcase! }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true, uniqueness: { scope: :email }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { scope: :email }
   validates :password, presence: true, length: { minimum: 5 }
   has_many :websites
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,57 @@ RSpec.describe User, type: :model do
     @user = FactoryGirl.create(:user)
   end
 
+########## Mixed Validation ##########
+
   it 'is valid with email and password' do
     expect(@user).to be_valid
   end
+
+ ########## Email Validation ##########
 
   it 'is invalid without email' do
     @user.email = nil
     @user.valid?
     expect(@user.errors[:email]).to include("can't be blank")
   end
+
+  it 'is invalid if email is too long' do
+    @user.email = 'a' * 244 + '@example.com'
+    expect(@user).to_not be_valid
+  end
+
+  it 'should accept valid email address' do
+    valid_addresses = %w[user@example.com USER@foo.com U_S-ER@foo.bar.org
+                        first.last@foo.jp first+last@foo.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).to be_valid
+    end
+  end
+
+  it 'should reject invalid email address' do
+    invalid_addresses = %w[user@example,com user_at_sign.com user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).to_not be_valid
+    end
+  end
+
+  it 'is invalid with duplicate email address' do
+    user = FactoryGirl.build(:user, email: @user.email)
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
+
+  it 'must save email address in lower case' do
+    mixed_case_email = 'Admin@EXAmple.com'
+    @user.email = mixed_case_email
+    @user.save
+    expect(mixed_case_email.downcase).to eq(@user.reload.email)
+  end
+
+########## Password Validation ##########
 
   it 'is invalid without password' do
     @user.password = nil
@@ -30,16 +72,12 @@ RSpec.describe User, type: :model do
     expect(@user).to be_valid
   end
 
-  it 'is invalid if doesnot contain atleast 5 characters long password' do
+  it 'is invalid if less than 5 characters' do
     @user.password = 'four'
     expect(@user).to_not be_valid
   end
 
-  it 'is invalid with duplicate email address' do
-    user = FactoryGirl.build(:user, email: @user.email)
-    user.valid?
-    expect(user.errors[:email]).to include('has already been taken')
-  end
+########## Other Validation ##########
 
   it 'has many websites' do
     user1 = FactoryGirl.create(:user, email: 'admin1@example.com')
